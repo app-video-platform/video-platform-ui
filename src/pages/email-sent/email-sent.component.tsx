6bcc3ed76f61c4f47214ba197751e775f6c45276
Alexandru Bej <alexbej07@gmail.com>
2025-03-03 17:10:23 +0100

    fix tsc error

--- src/pages/email-sent/email-sent.component.tsx
+++ src/pages/email-sent/email-sent.component.tsx
@@ -5,7 +5,11 @@ import './email-sent.styles.scss';
 
 const EmailSent: React.FC = () => (
   <div className='email-sent-message-container'>
-    <CiCircleCheck color='green' size={100} />
+    {React.createElement(CiCircleCheck as React.FC<{ color: string; size: number }>, {
+      color: 'green',
+      size: 100,
+    })}
+    {/* <CiCircleCheck color='green' size={100} /> */}
 
     <h2>Check your email</h2>
     <p>We’ve sent a verification email to your address. Please follow the link in the email to verify your account.</p>
