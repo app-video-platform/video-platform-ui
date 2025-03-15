96b48d59a871d393d229b33a7d8ded6db55d9e2d
Alexandru Bej <alexbej07@gmail.com>
2025-03-15 18:48:40 +0100

    user page preview

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,4 +3,11 @@ declare global {
     google?: typeof google;
   }
 }
+
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.svg';
+
 export { };
