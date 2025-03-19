34676b491ff6ada80700b52ffff1941285fea732
Alexandru Bej <alexbej07@gmail.com>
2025-03-19 16:47:37 +0100

    fix linting errors

--- src/galactica-app/onboarding/multi-step-form/step-five/step-five.component.tsx
+++ src/galactica-app/onboarding/multi-step-form/step-five/step-five.component.tsx
@@ -5,7 +5,11 @@ import './step-five.styles.scss';
 const StepFive: React.FC = () => (
   <div>
     <h1>Your personal data has been changed!</h1>
-    <p>Now, you can continue your onboarding process, where you get to go through the dashboard, products, and analytics, or you can skip that and go straight to using the app.</p>
+    <p>
+      Now, you can continue your onboarding process, where you get to go through
+      the dashboard, products, and analytics, or you can skip that and go
+      straight to using the app.
+    </p>
   </div>
 );
 
