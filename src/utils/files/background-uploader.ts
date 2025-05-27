2dc535b37ed695de5e0aa77b25d76fd5612427dc
Alexandru Bej <alexbej07@gmail.com>
2025-05-27 21:44:34 +0200

    fix lint errors

--- src/utils/files/background-uploader.ts
+++ src/utils/files/background-uploader.ts
@@ -1,7 +1,7 @@
-import { Sectiunile } from "../../api/products-api";
-import { IFilesWithSection } from "../../galactica-app/products/create-product/create-product.component";
-import { DownloadSection } from "../../models/product/download-section";
-import { uploadFileToSection } from "./upload-file";
+import { Sectiunile } from '../../api/products-api';
+import { IFilesWithSection } from '../../galactica-app/products/create-product/create-product.component';
+import { DownloadSection } from '../../models/product/download-section';
+import { uploadFileToSection } from './upload-file';
 
 export const uploadFilesInBackground = async (
   uploadedFiles: IFilesWithSection[],
@@ -19,7 +19,7 @@ export const uploadFilesInBackground = async (
 
   const uploadPromises = uploadedFiles.flatMap(({ sectionLocalId, files }) => {
     const backendId = localIdToBackendId.get(sectionLocalId);
-    if (!backendId) return [];
+    if (!backendId) { return []; }
 
     return files.map(file =>
       uploadFileToSection(backendId, file).then(
