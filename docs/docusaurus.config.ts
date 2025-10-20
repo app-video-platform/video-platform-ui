ca4f0cd9030b44ff76b9d6ccaa6ea49cbd33b984
Alexandru Bej <alexbej07@gmail.com>
2025-10-20 19:02:20 +0200

    config docusaurus url

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -18,7 +18,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://app.serious-debauchery.click',
+  url: 'https://luxury-klepon-a62307.netlify.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
